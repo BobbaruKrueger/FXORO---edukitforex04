3ad128aa578bf5a86f736632e5d4a2e5a7261dbe
Bobbaru <bobbaru_krueger@yahoo.com>
2018-11-05 13:38:35 +0200

    backup

---
+++ v2/it/js/scripts.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function ($) {
+	
+//	var s2 = $('#section2.formSection');
+//	if (screen.width > 750) {
+//		s2.css("margin-top", "-" + s2.height() + "px" );
+//		$( window ).resize(function() {
+//			s2.css("margin-top", "-" + s2.height() + "px" );
+//		});
+//	}
+
+});
